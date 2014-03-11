020b9838344a874d5f7c09a35a9d6393127e44e4
Josh Adams <josh@isotope11.com>
2014-03-10 23:15:14 -0500

    Update sigil syntax, tweak elixir version

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Markdown.Mixfile do
   def project do
     [ app: :markdown,
       version: @version,
-      elixir: "~> 0.12.0",
+      elixir: "~> 0.13.0-dev",
       compilers: [:hoedown, :elixir, :app],
       deps: deps ]
   end
